6545faf62ec3081540304e513e5dfefc56588485
Gitax18 <lusifer0418@gmail.com>
2025-04-13 20:49:28 +0530

    tied up the app together

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { DatabaseModule } from './database/database.module';
+import { CommunicationsModule } from './communications/communications.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: `.env.${process.env.NODE_ENV}`,
     }),
+    AuthModule,
+    UsersModule,
+    DatabaseModule,
+    CommunicationsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ResponseInterceptor } from './interceptors/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Interceptor to format outgoing responses
+  app.useGlobalInterceptors(new ResponseInterceptor());
   await app.listen(process.env.PORT ?? 8080);
 }
 bootstrap();
