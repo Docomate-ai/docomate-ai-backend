1cd5b5de21b7e8d721dd3229dc8efb541fdce91c
Gitax18 <lusifer0418@gmail.com>
2025-04-13 20:49:16 +0530

    add interceptor to structure responses

---
+++ src/interceptors/response.interceptor.ts
@@ -0,0 +1,29 @@
+// response.interceptor.ts
+
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable()
+export class ResponseInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const ctx = context.switchToHttp();
+    const response = ctx.getResponse();
+    return next.handle().pipe(
+      map((data) => {
+        const statusCode = response.statusCode;
+
+        return {
+          message: data.message ?? 'Success',
+          data: data.data ?? [],
+          statusCode,
+        };
+      }),
+    );
+  }
+}
