af7a15752df56300cf07e9102f18c82fbbfed38d
Gitax18 <lusifer0418@gmail.com>
2025-04-13 20:48:28 +0530

    add database provider

---
+++ src/database/database.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigService, ConfigModule } from '@nestjs/config';
+import { User, TempUser } from '../entities';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mongodb',
+        url: configService.get<string>('MONGO_URI'),
+        database: 'docomate-dev',
+        synchronize: true,
+        entities: [User, TempUser],
+      }),
+    }),
+  ],
+})
+export class DatabaseModule {}
