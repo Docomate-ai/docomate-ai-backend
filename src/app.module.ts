cb4e6373d5ac2d8bddf00edf3b779f51669b19c9
Gitax18 <lusifer0418@gmail.com>
2025-04-15 00:38:34 +0530

    stitch all modules together

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,16 +6,23 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
 import { CommunicationsModule } from './communications/communications.module';
+import { AiModule } from './ai/ai.module';
+import { ProjectsModule } from './projects/projects.module';
+import { B2Module } from './b2/b2.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: `.env.${process.env.NODE_ENV}`,
+      isGlobal: true,
     }),
     AuthModule,
     UsersModule,
     DatabaseModule,
     CommunicationsModule,
+    AiModule,
+    ProjectsModule,
+    B2Module,
   ],
   controllers: [AppController],
   providers: [AppService],
