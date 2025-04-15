d1fa7e9739b2fbd64afd85d7150398e741ccfcd7
Gitax18 <lusifer0418@gmail.com>
2025-04-15 23:23:38 +0530

    add projectModule

--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -3,14 +3,20 @@ import { ProjectsService } from './projects.service';
 import { ProjectsController } from './projects.controller';
 import { B2Module } from 'src/b2/b2.module';
 import { UsersModule } from 'src/users/users.module';
-import { UsersService } from 'src/users/users.service';
-import { ProjectRepository } from './projects.repository';
+import { ProjectsRepository } from './projects.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Project } from '../entities';
+import { AiModule } from 'src/ai/ai.module';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
-  providers: [ProjectsService, ProjectRepository],
+  providers: [ProjectsService, ProjectsRepository],
   controllers: [ProjectsController],
-  imports: [B2Module, UsersModule, TypeOrmModule.forFeature([Project])],
+  imports: [
+    B2Module,
+    UsersModule,
+    TypeOrmModule.forFeature([Project]),
+    AiModule,
+  ],
 })
 export class ProjectsModule {}
