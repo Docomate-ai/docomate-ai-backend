52552f0f27a596fd40c8c5accde7d052bb8aa291
Gitax18 <lusifer0418@gmail.com>
2025-04-15 23:23:21 +0530

    add projectController

--- src/projects/projects.controller.ts
+++ src/projects/projects.controller.ts
@@ -1,30 +1,55 @@
 import {
   Body,
   Controller,
+  Delete,
   Param,
   Post,
-  Query,
+  Session,
   UseGuards,
   UsePipes,
 } from '@nestjs/common';
 import { AuthGuard } from 'src/guards/authorized.guard';
 import { AddProjectDto } from './dtos/add-project.dto';
 import { ValidateRepoPipe } from './pipes/validateRepoUrl.pipe';
+import { ProjectsService } from './projects.service';
+import { JwtService } from '@nestjs/jwt';
+import { DeleteProjectDto } from './dtos/delete-project.dto';
 
 @UseGuards(AuthGuard)
 @Controller('project')
 export class ProjectsController {
-  @Post('projects')
-  getAllProjects() {
-    return 'all projects';
-  }
+  constructor(
+    private projectsService: ProjectsService,
+    private jwtService: JwtService,
+  ) {}
 
-  @Post(':id')
-  getProject() {
-    return 'project with id';
+  @Post('projects')
+  async getAllProjects(@Session() session: any) {
+    const { email } = this.jwtService.verify(session.token);
+    return this.projectsService.getAllProject(email);
   }
 
   @Post()
   @UsePipes(ValidateRepoPipe)
-  addProject(@Body() body: AddProjectDto) {}
+  async addProject(@Body() body: AddProjectDto, @Session() session: any) {
+    const { email, sub: userId } = this.jwtService.verify(session.token);
+    return this.projectsService.addProject(
+      body.repoUrl,
+      body.projectName,
+      email,
+      userId,
+    );
+  }
+
+  @Delete()
+  async deleteProject(@Body() body: DeleteProjectDto, @Session() session: any) {
+    const { sub: userId } = this.jwtService.verify(session.token);
+    return this.projectsService.deleteProject(body.projectName, userId);
+  }
+
+  @Post(':id')
+  getProject(@Param('id') projectId: string, @Session() session: any) {
+    const { sub: userId } = this.jwtService.verify(session.token);
+    return this.projectsService.getProjectById(projectId, userId);
+  }
 }
