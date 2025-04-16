eccae941ce0fc12a4c78e9ff51c28765d5adc17e
Gitax18 <lusifer0418@gmail.com>
2025-04-16 08:37:52 +0530

    add server status endpoint at / (for testing)

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,17 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private initAt: number;
+  constructor() {
+    this.initAt = Date.now();
+  }
+  serverStatus() {
+    const status = {
+      name: 'Docomate AI Nest Server',
+      description:
+        'Docomate AI is a tool to create github readmes, documentation and to talk with open source github codebases.',
+      initAt: this.initAt,
+    };
+    return status;
   }
 }
