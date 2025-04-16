e1973b33438752652936a98348165b08c879f339
Gitax18 <lusifer0418@gmail.com>
2025-04-16 11:18:55 +0530

    globlaize res format interceptor

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -14,9 +14,7 @@ import { AuthService } from './auth.service';
 import { VerifyAccountDto } from './dtos/verify-account.dto';
 import { LoginAccountDto } from './dtos/login-account.dto';
 import { AuthGuard } from 'src/guards/authorized.guard';
-import { ResponseInterceptor } from 'src/interceptors/response.interceptor';
 
-@UseInterceptors(ResponseInterceptor)
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Interceptor to format outgoing responses
-  // app.useGlobalInterceptors(new ResponseInterceptor());
+  app.useGlobalInterceptors(new ResponseInterceptor());
 
   // cookie-session middleware
   app.use(
