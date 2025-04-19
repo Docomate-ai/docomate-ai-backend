d92fd03abd7ce4dadd09cd2bf4cc788b07dd2889
Gitax18 <lusifer0418@gmail.com>
2025-04-20 00:33:01 +0530

    remove b2 and add Content

--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -1,21 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ProjectsService } from './projects.service';
 import { ProjectsController } from './projects.controller';
-import { B2Module } from 'src/b2/b2.module';
 import { UsersModule } from 'src/users/users.module';
 import { ProjectsRepository } from './projects.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Project } from '../entities';
+import { Content, Project } from '../entities';
 import { AiModule } from 'src/ai/ai.module';
-import { JwtService } from '@nestjs/jwt';
 
 @Module({
   providers: [ProjectsService, ProjectsRepository],
   controllers: [ProjectsController],
   imports: [
-    B2Module,
     UsersModule,
-    TypeOrmModule.forFeature([Project]),
+    TypeOrmModule.forFeature([Project, Content]),
     AiModule,
   ],
   exports: [ProjectsRepository],
