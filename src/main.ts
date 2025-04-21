be8c160490183f678aac1731e6575e31c218abd3
Gitax18 <lusifer0418@gmail.com>
2025-04-21 23:59:44 +0530

    dynamic allow frontend origins

--- src/main.ts
+++ src/main.ts
@@ -27,11 +27,26 @@ async function bootstrap() {
     }),
   );
 
+  // app.enableCors({
+  //   origin: [
+  //     'http://localhost:5173',
+  //     'https://docomate-ai.github.io/docomate-ai-frontend',
+  //   ],
+  //   credentials: true,
+  // });
+
   app.enableCors({
-    origin: [
-      'http://localhost:5173',
-      'https://docomate-ai.github.io/docomate-ai-frontend',
-    ],
+    origin: (origin, callback) => {
+      const allowedOrigins = [
+        'http://localhost:5173',
+        'https://docomate-ai.github.io',
+      ];
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     credentials: true,
   });
 
